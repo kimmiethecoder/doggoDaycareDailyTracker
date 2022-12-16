20fc015da363f3664df5713081010b8f3145b6d7
Kimberly Rodriguez <kimmiethecoder@gmail.com>
2022-12-16 16:11:36 -0500

    add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        {location.pathname === '/' && (<Button color={showAdd ? 'teal' : 'purple'} 
+        {location.pathname === '/' && (<Button color={showAdd ? 'red' : 'teal'} 
         text={showAdd ? 'Close' : 'Add'} 
         onClick={onAdd} />)}
     </header>
